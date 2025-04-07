d6a6f64cff5aefc28c935e46b28b79cbc387fc09
Abe <abe.hu@docusign.com>
2025-04-07 16:14:09 -0700

    sync-latest
    Signed-off-by: Abe <abe.hu@docusign.com>

--- widgets/@1fe/starter-kit/1.0.1/contract.d.ts
+++ widgets/@1fe/starter-kit/1.0.1/contract.d.ts
@@ -1,6 +1,6 @@
 import { PlatformPropsType } from '@devhub/1fe-shell';
 export type HostPropsContract = {};
-export type RootProps = {
+export type WidgetProps = {
     host: HostPropsContract;
     platform: PlatformPropsType;
 };
