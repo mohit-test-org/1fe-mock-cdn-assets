8ce40c39ab62f2eb87c0de61ae5bdaa2f2865757
abe.hu <abe.hu@docusign.com>
2025-05-27 22:34:39 -0700

    remove antd externals from bundles

--- integration/widgets/@1fe/bathtub/1.0.0/store/app/thunks.d.ts
+++ integration/widgets/@1fe/bathtub/1.0.0/store/app/thunks.d.ts
@@ -1,6 +1,5 @@
-import { Widget } from '../../services/widget';
 export declare const appThunks: {
-    loadWidgets: import("@reduxjs/toolkit").AsyncThunk<(string | Widget[])[], void, {
+    loadWidgets: import("@reduxjs/toolkit").AsyncThunk<any[], void, {
         state?: unknown;
         dispatch?: import("redux").Dispatch;
         extra?: unknown;
